56a969a5b20f41b6975cc045f70d316b3c305eec
dpgithubRepo <durga.pras10@gmail.com>
2022-02-08 03:25:13 +0530

    print digits of a number

---
+++ code/src/day3/PrintDigitsReverse.java
@@ -0,0 +1,36 @@
+package day3;
+import java.util.Scanner;
+/**
+ * 
+ * given a number, print the digits of it.
+ * 
+ * example: 87612 => 2,1,6,7,8
+ * 
+ * @author durga prasad
+ *
+ */
+public class PrintDigitsReverse {
+  
+	public static void printRecursive(int n) {
+		if(n<=0) return;
+		
+		System.out.println(n%10);
+		n = n/10;
+		printRecursive(n);
+	}
+	
+	public static void printIterative(int n) {
+		while(n>0) {
+			System.out.println(n%10);
+			n  = n/10;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		printRecursive(n);
+		printIterative(n);
+		scanner.close();
+	}
+}
