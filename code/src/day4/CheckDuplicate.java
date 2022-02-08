ca5c243db7f62d0f70a8e33f33612afaae3bb805
dpgithubRepo <durga.pras10@gmail.com>
2022-02-08 20:29:41 +0530

    added the comment for sort

--- code/src/day4/CheckDuplicate.java
+++ code/src/day4/CheckDuplicate.java
@@ -15,7 +15,7 @@ public class CheckDuplicate {
 	
 	public static boolean hasDuplicates(int[] arr) {
 		boolean flag = false;
-		Arrays.sort(arr);
+		Arrays.sort(arr);  //Arrays.sort(Object[]) is based on the TimSort algorithm, giving us a time complexity of O(n log(n)).
 		
 		for(int i = 0; i < arr.length -2; i++) {
 			if(arr[i] == arr[i+1]) {
