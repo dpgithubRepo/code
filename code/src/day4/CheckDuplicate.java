f6d2f868ba733dc907b8130b7a4e18e992c1a931
dpgithubRepo <durga.pras10@gmail.com>
2022-02-08 20:16:58 +0530

    check if dulicate in array

---
+++ code/src/day4/CheckDuplicate.java
@@ -0,0 +1,47 @@
+package day4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+/**
+ * 
+ * given an unsorted array check if any duplicates with nlogn
+ * 
+ * @author durga prasad
+ *
+ */
+public class CheckDuplicate {
+	
+	public static boolean hasDuplicates(int[] arr) {
+		boolean flag = false;
+		Arrays.sort(arr);
+		
+		for(int i = 0; i < arr.length -2; i++) {
+			if(arr[i] == arr[i+1]) {
+				flag = true;
+				System.out.println(arr[i] + " is duplicated");
+				break; 
+			}
+		}
+		
+		return flag;
+	}
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i = 0 ; i < n; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		scanner.close();
+		
+		System.out.println(hasDuplicates(arr));
+		
+	}
+
+}
